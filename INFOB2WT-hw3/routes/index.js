b428c5053c50c732b19f890098dd4bb939f23b5c
Quinten <qdamsma@gmail.com>
2025-03-19 13:23:43 +0100

    index route

--- INFOB2WT-hw3/routes/index.js
+++ INFOB2WT-hw3/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
-const path = require('path');
 
 router.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../views/index.html'));
