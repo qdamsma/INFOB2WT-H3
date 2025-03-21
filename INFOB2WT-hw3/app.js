718d8d755faba66092dbdc887211085fe07a95e9
André Rouwert Boer <arb220304@gmail.com>
2025-03-21 16:50:12 +0100

    about page works

--- INFOB2WT-hw3/app.js
+++ INFOB2WT-hw3/app.js
@@ -5,11 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,7 +18,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/about', aboutRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
