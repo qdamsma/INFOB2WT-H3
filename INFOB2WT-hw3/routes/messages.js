3a731abe7de53e846d029d5c548e06d9ad75f03a
André Rouwert Boer <arb220304@gmail.com>
2025-04-07 14:58:05 +0200

    small edits

--- INFOB2WT-hw3/routes/messages.js
+++ INFOB2WT-hw3/routes/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Message = require('../models/message');
+const Message = require('../models/messages');
 
 // POST /messages/send (Send a message)
 router.post('/send', (req, res) => {
